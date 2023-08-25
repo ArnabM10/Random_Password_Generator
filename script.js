6216308dd349658ee0875d5f7a87eb15a634fb55
Arnab Manna <110545895+ArnabM10@users.noreply.github.com>
2023-08-25 16:30:47 +0530

    Added all files

---
+++ script.js
@@ -0,0 +1,70 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyz"
+const numberSet =  "1234567890"
+const symbolSet = "~!@#$%^&**()_+/"
+
+
+//<=====================Selectors==============>
+
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput =  document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
+
+
+// Creating a function which returns random data from dataset
+const getRandomData = (dataSet) =>{
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+
+
+const generatePassword = (password = "") =>{
+    if(upperInput.checked)
+    {
+        password += getRandomData(upperSet)
+    }
+    if(lowerInput.checked){
+        password += getRandomData(lowerSet);
+    }
+    if(numberInput.checked){
+        password += getRandomData(numberSet);
+    }
+    if (symbolInput.checked){
+        password += getRandomData(symbolSet);
+    }
+
+
+    if(password.length < totalChar.value)
+    {
+        // calling recursive function
+        return generatePassword(password)
+    }
+    passBox.innerText = truncuateString(password, totalChar.value);
+}
+generatePassword();
+
+document.getElementById("btn").addEventListener(
+    "click",
+    function()
+    {
+        generatePassword();
+    }
+)
+
+
+function truncuateString(str, num)
+{
+    if(str.length > num)
+    {
+        let subStr = str.substring(0, num);
+        return subStr;
+    }
+    else
+    {
+        return str;
+    }
+}
